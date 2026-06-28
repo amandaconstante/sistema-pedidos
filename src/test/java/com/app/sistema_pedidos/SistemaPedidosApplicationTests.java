efcfd9a751324351badb073d45a112e194fe0bc6
Amanda <89360779+amandaconstante@users.noreply.github.com>
2026-06-28 17:50:29 -0300

    chore: commit inicial gerado pelo spring initializr

---
+++ src/test/java/com/app/sistema_pedidos/SistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sistema_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
