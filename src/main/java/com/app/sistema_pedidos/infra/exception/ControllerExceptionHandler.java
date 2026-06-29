8ab16f5c8b725edef8df650785edaf8e2da29969
Amanda <89360779+amandaconstante@users.noreply.github.com>
2026-06-29 12:26:55 -0300

    feat: implementa ControllerExceptionHandler para formatar json de validação

---
+++ src/main/java/com/app/sistema_pedidos/infra/exception/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.app.sistema_pedidos.infra.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError field) {
+            this(field.getField(), field.getDefaultMessage());
+        }
+    }
+}
